11be112e2dc51231f9f08b0943672e6e9ef35648
Labret <jfr.labret@hotmail.com>
2024-07-28 13:40:47 +0200

    feat: add database connection

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 
 import hikari
+import asyncpg
 
-from spike_bot import Spike
+from spike_bot import Spike, DatabaseManager
 
 
 def main():
@@ -13,6 +14,13 @@ def main():
 
     @bot.listen(hikari.StartedEvent)
     async def on_start(event: hikari.StartedEvent) -> None:
+        postgres_password = os.environ["POSTGRES_PASSWORD"]
+        postgres_url = f'postgres://postgres:{postgres_password}@postgres:5432/spike'
+        pool = await asyncpg.create_pool(postgres_url)
+        print("Database pool acquired.")
+        dbm = DatabaseManager(pool)
+        bot.dbm = dbm
+        await bot.dbm.create_tables()
         print("Bot started successfully!")
 
     bot.load_extensions_from("./spike_bot/extensions")
