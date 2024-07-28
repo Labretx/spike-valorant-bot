d5c38d79788eecc0fc31fdbb6db8ab271861db93
Labret <jfr.labret@hotmail.com>
2024-07-28 13:16:35 +0200

    fix: postgres data types

--- spike_bot/database/queries.py
+++ spike_bot/database/queries.py
@@ -1,21 +1,21 @@
 create_servers_table_query = """
         CREATE TABLE IF NOT EXISTS servers (
-            server_id LONG UNIQUE PRIMARY KEY,
+            server_id BIGINT UNIQUE PRIMARY KEY,
             setup_completed BOOLEAN,
             main_embed_color INTEGER,
-            listener_id LONG
+            listener_id BIGINT
         )""".strip()
 
 create_server_roles_table_query = """
         CREATE TABLE IF NOT EXISTS server_roles (
-            server_id_FK LONG REFERENCES servers (server_id),
+            server_id_FK BIGINT REFERENCES servers (server_id),
             role_name VARCHAR,
-            role_id LONG
+            role_id BIGINT
         )""".strip()
 
 create_accounts_table_query = """
         CREATE TABLE IF NOT EXISTS accounts (
-            discord_id LONG UNIQUE PRIMARY KEY,
+            discord_id BIGINT UNIQUE PRIMARY KEY,
             val_account_id VARCHAR UNIQUE,
             account_name VARCHAR,
             region VARCHAR
@@ -25,18 +25,18 @@ create_rank_info_table_query = """
         CREATE TABLE IF NOT EXISTS rank_info (
             account_id_FK VARCHAR UNIQUE REFERENCES accounts (val_account_id),
             rank_name VARCHAR,
-            rank_num SHORT,
+            rank_num SMALLINT,
             peak_rank VARCHAR,
             peak_season VARCHAR,
             rating INTEGER,
-            mmr_change SHORT,
+            mmr_change SMALLINT,
             main_agent VARCHAR
         )""".strip()
 
 create_server_association_table_query = """
         CREATE TABLE IF NOT EXISTS server_association (
-            account_id_FK LONG REFERENCES accounts (discord_id),
-            server_id_FK LONG REFERENCES servers (server_id)
+            account_id_FK BIGINT REFERENCES accounts (discord_id),
+            server_id_FK BIGINT REFERENCES servers (server_id)
         )""".strip()
 
 table_creation_queries = [
